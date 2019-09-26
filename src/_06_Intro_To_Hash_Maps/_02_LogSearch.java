d7a9579999a9e369af2cffb0b8070f464d4c6fb2
League Student <league@iMac-7.attlocal.net>
2019-09-25 19:57:17 -0700

    String_Integer_Pair, IntroToHashMaps, LogSearch(IP)

--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,7 +1,15 @@
 package _06_Intro_To_Hash_Maps;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class _02_LogSearch {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_LogSearch implements ActionListener {
   /* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
 	 * Create a GUI with three buttons. 
@@ -29,5 +37,66 @@ public class _02_LogSearch {
 	 * 				is not in the list. 
 	 *
 	 * */
+	
+	HashMap<Integer, String> log = new HashMap<Integer, String>();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton("Add Entry");
+	JButton search = new JButton("Search By ID");
+	JButton view = new JButton("View List");
+	
+	public static void main(String[] args) {
+		_02_LogSearch ls = new _02_LogSearch();
+		ls.setup();
+		ls.run();
+	}
+	
+	void run() {
+		
+	}
+	
+	void setup() {
+		frame.add(panel);
+		panel.add(add);
+		panel.add(search);
+		panel.add(view);
+		add.addActionListener(this);
+		search.addActionListener(this);
+		view.addActionListener(this);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		log.put(0, "test0");
+		log.put(1, "test1");
+		log.put(2, "test2");
+		log.put(4, "test3");
+		log.put(5, "test4");
+		log.put(6, "test5");
+		log.put(256, "test6");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource().equals(add)) {
+			log.put(Integer.parseInt(JOptionPane.showInputDialog("Insert ID Number")), JOptionPane.showInputDialog("Insert Name"));
+		}else if(e.getSource().equals(search)) {
+			JOptionPane.showMessageDialog(null, log.get(Integer.parseInt(JOptionPane.showInputDialog("Insert ID Number"))));
+		}else if(e.getSource().equals(view)) {
+			String s = "";
+			for (int i = 0; i <= log.size(); i++) {
+				System.out.println(i);
+				if(log.keySet().contains(i)) {
+				s = s + "ID: " + i + " Name: " + log.get(i) + "\n";
+				System.out.println("//" + i + " is valid");
+				}
+			}
+			JOptionPane.showMessageDialog(null, s);
+			System.out.println("--");
+		}
+	}
+	
+	
+	
 	
 }
